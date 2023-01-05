ea5f7a4ae6b645369791d3ec5ce1514b404cb6ad
Mariia <m.goncharenkoo@icloud.com>
2023-01-05 00:52:52 -0500

    fixed redirecting page after logout

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,11 +1,15 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 function NavBar({ user, setUser }) {
+
+  const navigate = useNavigate();
+
   function handleLogoutClick() {
     fetch("/logout", { method: "DELETE" }).then((r) => {
       if (r.ok) {
         setUser(null);
+        navigate('/');
       }
     });
   }
