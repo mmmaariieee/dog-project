4d83f7a404d8aaef31fbd1cd217097b7ba10d088
Mariia <m.goncharenkoo@icloud.com>
2023-01-06 18:07:15 -0500

    added readme

--- client/src/components/Welcome.js
+++ client/src/components/Welcome.js
@@ -9,7 +9,10 @@ function Welcome({ user }) {
     return (
         <>
             {user ? (
-                <h1 className="center">Welcome, {user.username}!</h1>
+                <div>
+                    <img className="center-button large-img" src='https://github.com/mmmaariieee/dog-project/blob/main/client/public/dogs.PNG?raw=true' />
+                    <h1 className="center">Welcome, {user.username}!</h1>
+                </div>
             ) : (
                 <h1 className="center">Please Login or Sign Up</h1>
             )}
